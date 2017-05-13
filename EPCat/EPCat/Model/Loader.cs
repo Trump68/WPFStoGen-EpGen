03029286f0bed3e96e1edfe87697794377c0d2bc
Trump68 <trump68@list.ru>
2017-05-13 06:10:13 +0300

    11

---
+++ EPCat/EPCat/Model/Loader.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPCat.Model
+{
+    public class Loader
+    {
+        static string c_UpdateFolder = "UPDATE FOLDER ";
+        static string c_LoadCatalog = "LOAD CATALOG ";
+        static string c_MoveFiles = "MOVE FILES ";
+        static string c_SynchFiles = "SYNCH FILES ";
+        static string c_CreatePassport = "CREATE PASSPORT ";
+
+
+        private List<EpItem> Source;
+        public List<EpItem> ProcessScriptFile(List<EpItem> sourceList)
+        {
+            Source = sourceList;
+            List<string> LogList = new List<string>(File.ReadAllLines("commandscript.txt"));
+            foreach (var item in LogList)
+            {
+                if (item.TrimStart().StartsWith("//")) continue;
+                ParseLine(item);
+            }
+            return Source;
+        }
+
+        private string CurrentCatalog;
+        internal void SaveCatalog()
+        {
+            if (string.IsNullOrEmpty(CurrentCatalog)) return;
+            System.Xml.Serialization.XmlSerializer serializer = new System.Xml.Serialization.XmlSerializer(typeof(List<EpItem>));
+            using (var writer = new StreamWriter(CurrentCatalog))
+            {
+                serializer.Serialize(writer, Source);
+            }
+        }
+
+        internal void ParseLine(string line)
+        {
+            line = line.Trim();
+            if (line.StartsWith(c_UpdateFolder))
+            {
+                UpdateFolder(line.Replace(c_UpdateFolder, string.Empty));
+            }
+            else if (line.StartsWith(c_LoadCatalog))
+            {
+                LoadCatalog(line.Replace(c_LoadCatalog, string.Empty));
+            }
+            else if (line.StartsWith(c_MoveFiles))
+            {
+                MoveFiles(line.Replace(c_MoveFiles, string.Empty));
+            }
+            else if (line.StartsWith(c_SynchFiles))
+            {
+                SynchFiles(line.Replace(c_SynchFiles, string.Empty));
+            }
+            else if (line.StartsWith(c_CreatePassport))
+            {
+                CreatePassport(line.Replace(c_CreatePassport, string.Empty));
+            }
+        }
+                    
+
+
+        // create passport
+        private void CreatePassport(string parameters)
+        {
+            string pathTo = parameters.ToLower();
+            if (!Directory.Exists(Path.GetPathRoot(pathTo))) return;
+            CreatePassportTo(pathTo);
+        }
+        private void CreatePassportTo(string ToPath)
+        {
+            List<string> files = Directory.GetFiles(ToPath, EpItem.p_PassportName, SearchOption.TopDirectoryOnly).ToList();
+
+            if (!files.Any())
+            {
+                EpItem item = new EpItem(0);
+                item.Name = Path.GetFileName(ToPath);
+                List<string> lines = EpItem.SetToPassport(item);
+                File.WriteAllLines(Path.Combine(ToPath, EpItem.p_PassportName), lines);
+
+            }
+
+            List<string> dirs = Directory.GetDirectories(ToPath).ToList();
+            foreach (var item in dirs)
+            {
+                CreatePassportTo(item);
+            }
+        }
+
+
+        // synch files
+        private void SynchFiles(string parameters)
+        {
+            string str = parameters.ToLower();
+            string[] pars = str.Split(';');
+            if (pars.Length == 3)
+            {
+                string mask = pars[0];
+                string pathFrom = pars[1];
+                string pathTo = pars[2];
+                if (!Directory.Exists(Path.GetPathRoot(pathTo))) return;
+                if (!Directory.Exists(pathFrom)) return;
+                SychFileFromTo(mask, pathFrom, pathTo, false, true);
+            }
+        }
+        
+
+
+        private void SychFileFromTo(string mask, string fromPath, string ToPath, bool isMove, bool isSynch)
+        {
+            List<string> files = mask.Split('|').SelectMany(filter => Directory.GetFiles(fromPath, filter, SearchOption.TopDirectoryOnly)).ToList();
+
+            if (files.Any())
+            {
+                bool existingCheck = true;
+                if (!Directory.Exists(ToPath))
+                {
+                    Directory.CreateDirectory(ToPath);
+                    existingCheck = false;
+                }
+                foreach (var item in files)
+                {
+                    string newFilePath = Path.Combine(ToPath, Path.GetFileName(item));
+                    bool fileExists = false;
+                    if (existingCheck)
+                    {
+                        if (File.Exists(newFilePath))
+                        {
+                            fileExists = true;
+                            DateTime oldcreation = File.GetLastWriteTime(newFilePath);
+                            DateTime newcreation = File.GetLastWriteTime(item);
+                            if (oldcreation == newcreation) continue;
+                        }
+                    }
+                    File.Copy(item, newFilePath, true);
+                    if (isMove)
+                    {
+                        File.Delete(item);
+                    }
+                }
+            }
+            if (isSynch)
+            {
+                if (Directory.Exists(ToPath))
+                {
+                    List<string> filesDest = mask.Split('|').SelectMany(filter => Directory.GetFiles(ToPath, filter, SearchOption.TopDirectoryOnly)).ToList();
+                    var filenamesSource = files.Select(x => Path.GetFileName(x)).ToList();
+                    foreach (var fn in filesDest)
+                    {
+                        string filenameDest = Path.GetFileName(fn);
+                        if (!filenamesSource.Contains(filenameDest)) File.Delete(fn);
+                    }
+
+                }
+            }
+            List<string> dirs = Directory.GetDirectories(fromPath).ToList();
+            foreach (var item in dirs)
+            {
+                string newPath = Path.Combine(ToPath, Path.GetFileName(item));
+                SychFileFromTo(mask, item, newPath, isMove, isSynch);
+            }
+            if (isSynch && Directory.Exists(ToPath))
+            {
+                List<string> dirsDest = Directory.GetDirectories(ToPath).ToList();
+                var dirnamesSource = dirs.Select(x => Path.GetFileName(x)).ToList();
+                foreach (var dn in dirsDest)
+                {
+                        string dirnameDest = Path.GetFileName(dn);
+                        if (!dirnamesSource.Contains(dirnameDest)) Directory.Delete(dn,true);
+                }
+            }
+        }
+
+        private void MoveFiles(string parameters)
+        {
+            string str = parameters.ToLower();
+            string[] pars = str.Split(';');
+            if (pars.Length == 3)
+            {
+                string mask = pars[0];
+                string pathFrom = pars[1];
+                string pathTo = pars[2];
+                if (!Directory.Exists(Path.GetPathRoot(pathTo))) return;
+                if (!Directory.Exists(pathFrom)) return;
+                SychFileFromTo(mask, pathFrom, pathTo, true, false);
+            }
+        }
+
+
+        private void LoadCatalog(string parameters)
+        {
+            string itemPath = parameters.ToLower();
+            if (File.Exists(itemPath))
+            {
+                List<EpItem> list = new List<EpItem>();
+                System.Xml.Serialization.XmlSerializer serializer = new System.Xml.Serialization.XmlSerializer(typeof(List<EpItem>));
+                try
+                {
+                    using (var sr = new StreamReader(itemPath))
+                    {
+                        list = serializer.Deserialize(sr) as List<EpItem>;
+                    }
+                }
+                catch { }
+                CurrentCatalog = itemPath;
+                Source = list;
+            }
+        }
+        private void UpdateFolder(string parameters)
+        {
+            string itemPath = parameters.ToLower();
+            List<string> passportList = Directory.GetFiles(itemPath, EpItem.p_PassportName).ToList();
+            foreach (var passport in passportList)
+            {
+                CreateUpdateFromPassort(passport);
+            }
+            List<string> dirList = Directory.GetDirectories(itemPath).ToList();
+            foreach (var dir in dirList)
+            {
+                UpdateFolder(dir);
+            }
+        }
+
+        internal void UpdateItem(EpItem item)
+        {
+            List<string> passportData = EpItem.SetToPassport(item);
+            File.WriteAllLines(item.ItemPath, passportData);
+        }
+
+        private void CreateUpdateFromPassort(string passportPath)
+        {
+            List<string> passport = new List<string>(File.ReadAllLines(passportPath));
+            if (passport != null)
+            {
+                EpItem item = EpItem.GetFromPassport(passport);
+                item.ItemPath = passportPath;
+
+                var existingItem = Source.Where(x => x.GID == item.GID).FirstOrDefault();
+                if (existingItem == null)
+                {
+                    Source.Add(item);
+                    UpdateItem(item);
+                }
+                else
+                {
+                    existingItem.UpdateFrom(item);
+                }
+            }
+        }
+
+
+    }
+
+}
---
+++ EPCat/EPCat/ViewModel/EpCatViewModel.cs
@@ -0,0 +1,69 @@
+﻿using DevExpress.Mvvm;
+using DevExpress.Mvvm.POCO;
+using EPCat.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace EPCat
+{
+    public class EpCatViewModel: ViewModelBase
+    {
+        Loader _Loader = new Loader();
+        public EpCatViewModel()
+        {
+
+        }
+
+        internal List<EpItem> _FolderList = new List<EpItem>();
+
+
+        //bool updateenabled = false;
+
+        internal void Close()
+        {
+            _Loader.SaveCatalog();           
+        }
+
+        private ObservableCollection<EpItem> _FolderListView = new ObservableCollection<EpItem>();
+        public ObservableCollection<EpItem> FolderListView
+        {
+            get
+            {               
+                return _FolderListView;
+            }
+            set
+            {
+                _FolderListView = value;
+            }
+        }
+
+
+        public EpItem CurrentFolder { get; set; }
+
+        public void ProcessScriptFile()
+        {
+            this._FolderList = _Loader.ProcessScriptFile(this._FolderList);
+            this.FolderListView = new ObservableCollection<EpItem>(this._FolderList);
+            RaisePropertyChanged(() => this.FolderListView);
+            
+        }
+
+        internal void UpdateCurrentItem()
+        {
+            //if (updateenabled)
+            //{
+                if (this.CurrentFolder != null)
+                {
+                    _Loader.UpdateItem(this.CurrentFolder);
+                }
+            //}
+        }
+    }
+
+}
