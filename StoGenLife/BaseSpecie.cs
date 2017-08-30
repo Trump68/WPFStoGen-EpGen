f76daa8b5f47cab2b06c7834a07e155d1b34ecf3
CSTBRODSA <CSTBRODSA@WPET-W7E-P00407>
2017-08-30 19:39:18 +0300

    added

---
+++ StoGenLife/BaseSpecie.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoGenLife
+{
+    public class BaseSpecie
+    {
+        // Public
+        public int EnergyStored { set; get; }
+        public int EnergyConsumption { set; get; }
+
+        public BaseSpecie()
+        {
+            this.IsLive = true;
+        }
+        public bool Live { get { return IsLive; } }
+        public bool Kill()
+        {
+           return this.Die();
+        }
+        public void Exists(int timeUnits)
+        {
+            this.EnergyStored = this.EnergyStored - (this.EnergyConsumption * timeUnits);
+            if (this.EnergyStored <= 0)
+                this.Die();
+        }
+
+        // Privates
+        private bool IsLive { set; get; }
+        private bool Die()
+        {
+            this.IsLive = false;
+            return this.IsLive;
+        }
+
+    }
+}
---
+++ StoGenLifeTests/BaseSpecieTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StoGenLife;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoGenLife.Tests
+{
+    [TestClass()]
+    public class BaseSpecieTests
+    {
+        [TestMethod()]
+        public void LiveDieTest()
+        {
+            BaseSpecie specie = new BaseSpecie();
+            Assert.IsTrue(specie.Live);
+            specie.Kill();
+            Assert.IsFalse(specie.Live);
+        }
+        [TestMethod()]
+        public void DieWithoutEnergyFeed()
+        {
+            BaseSpecie specie = new BaseSpecie();
+            specie.EnergyStored = 1000;
+            specie.EnergyConsumption = 10;
+            specie.Exists(10);
+            Assert.IsTrue(specie.Live);
+            specie.Exists(100);
+            Assert.IsFalse(specie.Live);
+        }
+    }
+}
