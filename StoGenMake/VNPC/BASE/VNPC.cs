22e5188ee751acdca5d17fba281ce34d67031cd5
CSTBRODSA <CSTBRODSA@WPET-W7E-P00407>
2017-09-08 08:55:51 +0300

    1

--- StoGenMake/VNPC/BASE/VNPC.cs
+++ StoGenMake/VNPC/BASE/VNPC.cs
@@ -12,5 +12,47 @@ namespace StoGenMake.Pers
     {
         public BaseScene Scene { set; get; }
         public virtual void PrepareScene() { }
+
+        public NPCVisualData Visual = new NPCVisualData();
+        public void SetPersVariablesData(List<string> datalist)
+        {
+            List<string> result = new List<string>();
+            if (datalist != null)
+            {
+                foreach (var item in datalist)
+                {
+                    string[] vals = item.Split(';');
+                    Visual.SetByName(vals[0].Trim(),vals[1].Trim());
+                }
+            }
+        }
     }
+
+    public class NPCVisualData
+    {
+        public static string ImageDialogMain = "PIC Main dialog";
+        List<VNPCVariable> variables = new List<VNPCVariable>();        
+        public IEnumerable<VNPCVariable> ByName(string name)
+        {
+            return variables.Where(x => x.Name == name);
+        }
+        public void SetByName(string name,string val)
+        {
+            ByName(name).ToList().ForEach(x => x.Value = val);
+        }
+    }
+
+    public class VNPCVariable
+    {
+        public string Name;
+        public string Description;
+        public string Value;
+        public VNPCVariable(string name, string val, string desc)
+        {
+            Name = name;
+            Description = desc;
+            Value = val;
+        }
+    }
+}
 }
---
+++ StoGenMake/VNPC/READY/GenericFem.cs
@@ -0,0 +1,50 @@
+﻿using StoGenMake.Elements;
+using StoGenMake.Scenes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoGenMake.Pers
+{
+    public class GenericFem : VNPC
+    {
+        public GenericFem():base()
+        {
+            this.Scene = new Scene02();
+            this.Name = "Generic Fem";
+            this.GID = Guid.Parse("{8639F388-030F-4689-B5EE-020D03F4F46A}");
+        }
+
+        public override void PrepareScene()
+        {
+            ScenCadre cadre = new ScenCadre(this.Scene);
+            cadre.Name = "Cadre 01";
+            cadre.Timer = 5 * 1000;
+
+            ScenElementImage image;
+            image = new ScenElementImage();
+            image.SizeX = 1600;
+            image.SizeY = 1500;
+            image.Name = Scene02.MainFace_01;
+            image.Opacity = 100;
+            image.IsOptional = false;
+            cadre.VisionList.Add(image);
+
+
+            ScenElementSound sound;
+            sound = new ScenElementSound();
+            sound.Name = Scene02.MainMusic;
+            sound.V = 0;
+            sound.Transition = "v.B.5000.10";
+            cadre.SoundList.Add(sound);
+
+            ScenElementText text = new ScenElementText();
+            text.Text = "fff";
+            cadre.TextList.Add(text);
+
+            this.Scene.Cadres.Add(cadre);
+        }
+    }
+}
